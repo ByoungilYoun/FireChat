2bd0b8671df66eb10df6b3d1f03d7ea76adc6ee7
YounByoungil <jimmy7525@naver.com>
2021-07-06 00:54:03 +0900

    Setup Messages TableView

--- FireChat/FireChat/Controller/ConversationsController.swift
+++ FireChat/FireChat/Controller/ConversationsController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ConversationsController : UIViewController {
   
   //MARK: - Properties
+  private let tableView = UITableView()
   
   //MARK: - Lifecycle
   override func viewDidLoad() {
@@ -21,6 +22,7 @@ class ConversationsController : UIViewController {
   private func configurUI() {
     view.backgroundColor = .white
     configureNavigationBar()
+    configureTableView()
   }
   
   // 네비게이션 속성
@@ -44,9 +46,39 @@ class ConversationsController : UIViewController {
     navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
   }
   
+  private func configureTableView() {
+    tableView.backgroundColor = .white
+    tableView.rowHeight = 80
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    tableView.tableFooterView = UIView()
+    tableView.delegate = self
+    tableView.dataSource = self
+    
+    view.addSubview(tableView)
+    tableView.frame = view.frame
+  }
   //MARK: - Selectors
   @objc func showProfile() {
     print("1234")
   }
+}
+
+  //MARK: - UITableViewDataSource
+extension ConversationsController : UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
   
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = "Test Cell"
+    return cell
+  }
+}
+
+  //MARK: - UITableViewDelegate
+extension ConversationsController : UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print(indexPath.row)
+  }
 }
